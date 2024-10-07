9fd767620ceb539b45d722f98ab83d6743fed18a
Son <sonit251203@gmail.com>
2024-10-07 10:50:09 +0700

    update login

--- lib/data/repository/auth/auth_repository.dart
+++ lib/data/repository/auth/auth_repository.dart
@@ -1,6 +1,7 @@
 import 'package:injectable/injectable.dart';
 import 'package:spotify/data/models/user/user_body.dart';
 import '../../../network/api_client.dart';
+import '../../../network/api_util.dart';
 import '../../models/object_response.dart';
 import '../../models/user/users.dart';
 
@@ -10,22 +11,14 @@ abstract class AuthRepository {
 
 @singleton
 class AuthRepositoryImpl extends AuthRepository {
-  final ApiClient apiClient;
-
-  AuthRepositoryImpl(this.apiClient);
-
+  ApiClient apiClient = ApiUtil.apiClient;
   @override
   Future<ObjectResponse<Auth>> login(AuthBody body) async {
     try {
-      final response = await apiClient.login(body.toJson());
-      final authData = Auth.fromJson(response);
-      return ObjectResponse<Auth>(
-        data: authData,
-      );
+      final res = await apiClient.login(body.toJson());
+      return res;
     } catch (e) {
-      return ObjectResponse<Auth>(
-        data: null,
-      );
+      rethrow;
     }
   }
 }
